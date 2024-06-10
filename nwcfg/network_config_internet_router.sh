1ded794d3138702e5850f24f2abb3f1006dee2ac
e60e256 <e60nayuta@g.ecc.u-tokyo.ac.jp>
2024-06-11 01:54:44 +0900

    internet router

--- nwcfg/network_config_internet_router.sh
+++ nwcfg/network_config_internet_router.sh
@@ -1,2 +1,3 @@
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
-# sysctl -w net.ipv6.conf.all.forwarding=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+
--- nwcfg/network_config_mobile_router.sh
+++ nwcfg/network_config_mobile_router.sh
@@ -28,12 +28,21 @@ ip link set eth0 netns NS
 # Assign eth0 to br0
 ip netns exec NS ip link set eth0 master br0
 
-#ip6tables -A FORWARD -i veth0 -j DROP
-#p netns exec NS ip6tables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1340
+ip6tables -A FORWARD -i veth0 -j DROP
+ip netns exec NS ip6tables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1340
 
 ip netns exec NS ip link set NS-veth1 promisc on
 ip netns exec NS ip link set eth0 promisc on
 ip netns exec NS ip link set dev br0 type bridge forward_delay 0
+
+
+echo "221 tunnel-2210" | tee -a /etc/iproute2/rt_tables
+echo "222 tunnel-2220" | tee -a /etc/iproute2/rt_tables
+ip -6 rule add from 2001:200:0:1cdc:1e60:0:2210:0/112 lookup tunnel-2210
+ip -6 rule add from 2001:200:0:1cdc:1e60:0:2220:0/112 lookup tunnel-2220
+ip -6 route add default via 2001:200:0:1cdc:1e60:0:2210:4 dev eth1 table tunnel-2210
+ip -6 route add default via 2001:200:0:1cdc:1e60:0:2220:4 dev eth2 table tunnel-2220
+
 
 # Bring up the veth interfaces
 ip netns exec NS ip link set eth0 up
